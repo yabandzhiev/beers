f0646085b30e968366c0ea78b3ab219b5a56cecb
yabandzhiev <yabandzhiev@abv.bg>
2022-06-22 20:14:29 +0300

    temp request

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react";
+import { getAllBeersRequest } from "../../api/beerRequests";
+
+import BeerItem from "../BeerItem/BeerItem";
+
 import "./Home.scss";
 
 const Home = () => {
+  const [beerObj, setBeerObj] = useState({});
+  useEffect(() => {
+    const request = async () => {
+      const result = await getAllBeersRequest();
+      if (result?.data) {
+        let data = [];
+        for (let beer of result.data) {
+          const { id, name, description, image_url } = beer;
+          data.push({ id, name, description, image_url });
+        }
+        setBeerObj(data);
+      }
+
+      return result;
+    };
+    request();
+  }, []);
+
   return (
     <div className="home">
-      <input type="text" name="search" />
-      <button>Search</button>
-      <div className="content">content</div>
+      <div className="search">
+        <input type="text" name="search" />
+        <button>Search</button>
+      </div>
+
+      <div className="content">
+        {Object.keys(beerObj).length > 0
+          ? beerObj.map((beer) => <BeerItem key={beer.id} data={beer} />)
+          : "No data"}
+      </div>
     </div>
   );
 };
