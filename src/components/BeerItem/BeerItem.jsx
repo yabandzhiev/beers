92c26bcfbc553967ce65982be3ffc480ce9d41a2
yabandzhiev <yabandzhiev@abv.bg>
2022-06-22 20:14:17 +0300

    Update BeerItem.jsx

--- src/components/BeerItem/BeerItem.jsx
+++ src/components/BeerItem/BeerItem.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import "./BeerItem.scss";
 
-const BeerItem = () => {
-  return <div>BeerItem</div>;
+const BeerItem = ({ data }) => {
+  const { name, description, image_url } = data;
+  return (
+    <div className="beer-item">
+      <img className="beer-item__image" src={image_url} alt="Beer Image" />
+      <div className="beer-item__info">
+        <h4 className="beer-item__info--title">{name}</h4>
+        <p className="beer-item__info--description">{description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default BeerItem;
