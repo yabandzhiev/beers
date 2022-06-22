659ce3e688ae89c179e94c83e7669d658d3e08ca
yabandzhiev <yabandzhiev@abv.bg>
2022-06-22 14:11:56 +0300

    update

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <input />
+      <button>Search</button>
+      <div className="content">content</div>
+    </div>
+  );
+};
+
+export default Home;
