cf42f999e4f8980d4235bebef7c61a62d076b388
yabandzhiev <yabandzhiev@abv.bg>
2022-06-22 10:35:28 +0300

    create requests

---
+++ src/api/beerRequests.js
@@ -0,0 +1,19 @@
+import { axios, GET_RANDOM_BEER } from "../constants/backend";
+
+const getAllBeersRequest = async () => {
+  try {
+    const beersData = await axios.get(`/`);
+    return beersData;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const getRandomBeerRequest = async () => {
+  try {
+    const randomBeerData = await axios.get(`${GET_RANDOM_BEER}`);
+    return randomBeerData;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
