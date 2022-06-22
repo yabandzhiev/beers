48dab624b3d038b263cd0e9145f85ee72b0c1bdf
yabandzhiev <yabandzhiev@abv.bg>
2022-06-22 14:12:22 +0300

    add outlet, fix styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,34 @@
+import { NavLink, Outlet } from "react-router-dom";
+
 import "./Header.scss";
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className="header__bar">
-        <span className="header__bar--logo">Beans Love Beers</span>
+    <>
+      <div className="header">
+        <div className="header__bar">
+          <span className="header__bar--logo">Beans Love Beers</span>
 
-        <div className="header__bar--buttons">
-          <span>Home</span>
-          <span>Favorites</span>
-          <span>Random Beer</span>
+          <div className="header__bar--buttons">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "")}
+              to="/"
+            >
+              <span>Home</span>
+            </NavLink>
+
+            <NavLink to="favourites">
+              <span>Favorites</span>
+            </NavLink>
+
+            <NavLink to="random">
+              <span>Random Beers</span>
+            </NavLink>
+          </div>
         </div>
       </div>
-    </div>
+      <Outlet />
+    </>
   );
 };
 
