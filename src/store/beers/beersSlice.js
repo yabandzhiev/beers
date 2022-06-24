c9e0304f330e375bf4a1328cfa176b7b947ffcc5
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-06-24 19:02:47 +0300

    Update beersSlice.js

--- src/store/beers/beersSlice.js
+++ src/store/beers/beersSlice.js
@@ -11,13 +11,20 @@ export const beersSlice = createSlice({
   name: "beers",
   initialState,
   reducers: {
+    setNewState: (state,action) => {
+      state.value.beers = action.payload;
+    }
+
     toggleFavourites: (state, action) => {
       const id = action.payload;
 
-      const foundBeer = state.value.beers.find((beer) => beer.id === id);
+      const foundBeer = state.value.favourites.find((beer) => beer.id === id);
 
-      if (foundBeer) {
+      if (!foundBeer) {
         state.value.favourites.push(foundBeer);
+      } else {
+        const index = state.value.favourites.indexOf(id);
+        state.value.favourites.splice(index, 1);
       }
     },
   },
