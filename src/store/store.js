5d071051e11b66a2302d7e97cf238160bc326904
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-06-24 20:48:54 +0300

    add imports

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,15 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import logger from "redux-logger";
 import {
-  configureStore,
-  combineReducers,
-  applyMiddleware,
-  compose,
-} from "@reduxjs/toolkit";
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import beerReducer from "./beers/beersSlice";
