7840c545c1fcacb1f32ccc2a7066e916e6b592da
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-06-24 21:06:49 +0300

    add store imports

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
+import { store, persistor } from "./store/store";
+
 import App from "./App";
 
 import "./index.css";
