82c40d92348b7c22bf5f25e9a389e10e82f8b9f2
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-06-26 16:50:34 +0300

    use new actions

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,55 +1,38 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { getAllBeersRequest } from "../../api/beerRequests";
 
 import { useBeerActionsDispatch } from "../../common/hooks/useActions";
 
-import BeerItem from "../BeerItem/BeerItem";
+import Content from "../Content/Content";
+import SearchBox from "../SearchBox/SearchBox";
 
 import "./Home.scss";
 
 const Home = () => {
-  const { setNewState } = useBeerActionsDispatch();
+  const { fetchData } = useBeerActionsDispatch();
   const beersObj = useSelector((state) => state.beers.value);
-  const beers = beersObj.beers;
+  const [userInput, setUserInput] = useState("");
 
+  //load data from api
   useEffect(() => {
-    request();
+    fetchData();
   }, []);
 
-  //request data from api
-  const request = async () => {
-    const result = await getAllBeersRequest();
-    if (result?.data) {
-      let data = [];
-      for (let beer of result.data) {
-        const { id, name, description, image_url } = beer;
-        data.push({ id, name, description, image_url });
-      }
-      //dispatch to set new state in redux store
-      setNewState(data);
-    }
-
-    return result;
+  const onInputChange = (e) => {
+    setUserInput(e.target.value);
   };
+
+  const filteredInputData = !userInput
+    ? beersObj.beers
+    : beersObj.beers.filter((beer) => {
+        return beer.name.toLowerCase().includes(userInput.toLowerCase());
+      });
 
   return (
     <div className="home">
-      <div className="search">
-        <input
-          type="text"
-          name="search"
-          className="input"
-          placeholder="Search for a beer..."
-        />
-        <button className="button">Search</button>
-      </div>
+      <SearchBox userInput={userInput} handler={onInputChange} />
 
-      <div className="content">
-        {Object.keys(beers).length > 0
-          ? beers.map((beer) => <BeerItem key={beer.id} data={beer} />)
-          : "No data"}
-      </div>
+      <Content filteredInputData={filteredInputData} />
     </div>
   );
 };
