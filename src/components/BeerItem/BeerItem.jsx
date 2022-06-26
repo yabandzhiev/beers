5f5bea90a6db6a2e22b000a2eaff8c6246a856dc
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-06-26 16:49:40 +0300

    Update BeerItem.jsx

--- src/components/BeerItem/BeerItem.jsx
+++ src/components/BeerItem/BeerItem.jsx
@@ -1,12 +1,27 @@
 import "./BeerItem.scss";
 
+import { useBeerActionsDispatch } from "../../common/hooks/useActions";
+
 const BeerItem = ({ data }) => {
-  const { name, description, image_url } = data;
+  const { toggleFavourites } = useBeerActionsDispatch();
+  const name = data?.name;
+  const description = data?.description;
+  const image_url = data?.image_url;
+  const favourite = data?.favourite;
+
+  const toggleFav = () => {
+    toggleFavourites(data);
+  };
+
   return (
     <div className="beer-item">
       <img className="beer-item__image" src={image_url} alt="Beer Image" />
       <div className="beer-item__info">
-        <span>&#x2605;&#x2606;</span>
+        {favourite ? (
+          <span onClick={toggleFav}>&#x2605;</span>
+        ) : (
+          <span onClick={toggleFav}>&#x2606;</span>
+        )}
         <h4 className="beer-item__info--title">{name}</h4>
         <p className="beer-item__info--description">{description}</p>
       </div>
