7e15315e32864e11ab83f9579afc0ed713649437
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-06-26 16:50:09 +0300

    replace HomeContent with Content

---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,20 @@
+import "./Content.scss";
+import BeerItem from "../BeerItem/BeerItem";
+
+const random = () => {
+  return Math.floor(Math.random() * 1138176387468176418);
+};
+
+const Content = ({ filteredInputData }) => {
+  return (
+    <div className="content">
+      {filteredInputData
+        ? filteredInputData.map((beer) => (
+            <BeerItem key={random()} data={beer} />
+          ))
+        : "No data"}
+    </div>
+  );
+};
+
+export default Content;
