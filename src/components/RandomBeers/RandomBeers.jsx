b7b86ce55a51bafdc75b752953e54fdb41c9c6ed
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-06-26 16:50:50 +0300

    add random beer page

--- src/components/RandomBeers/RandomBeers.jsx
+++ src/components/RandomBeers/RandomBeers.jsx
@@ -1,5 +1,19 @@
+import BeerItem from "../BeerItem/BeerItem";
+import { useBeerActionsDispatch } from "../../common/hooks/useActions";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
 const RandomBeers = () => {
-  return <div>RandomBeers</div>;
+  const { fetchRandomBeer } = useBeerActionsDispatch();
+  const randomBeer = useSelector((state) => state.beers.value.randomBeer);
+  console.log(randomBeer);
+  //dispatch action to get random beer
+  useEffect(() => {
+    fetchRandomBeer();
+  }, []);
+
+  return <BeerItem data={randomBeer[0]} />;
+  // return <div>random</div>;
 };
 
 export default RandomBeers;
