48c60d4dff8e1bcc1dcbd10bc8ab3bc18f9cc886
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-06-27 09:42:59 +0300

    fav button to component

--- src/components/BeerItem/BeerItem.jsx
+++ src/components/BeerItem/BeerItem.jsx
@@ -1,30 +1,16 @@
-import { useBeerActionsDispatch } from "../../common/hooks/useActions";
-
 import "./BeerItem.scss";
+import FavouriteButton from "./FavouriteButton/FavouriteButton";
 
 const BeerItem = ({ data }) => {
-  const { toggleFavourites } = useBeerActionsDispatch();
   const name = data?.name;
   const description = data?.description;
   const image_url = data?.image_url;
   const favourite = data?.favourite;
 
-  const toggleFav = () => {
-    toggleFavourites(data);
-  };
-
   return (
     <div className="beer-item">
       <img className="beer-item__image" src={image_url} alt="Beer Image" />
-      {favourite ? (
-        <span className="star" onClick={toggleFav}>
-          &#x2605;
-        </span>
-      ) : (
-        <span className="star" onClick={toggleFav}>
-          &#x2606;
-        </span>
-      )}
+      <FavouriteButton favourite={favourite} data={data} />
       <div className="beer-item__info">
         <h4 className="beer-item__info--title">{name}</h4>
         <p className="beer-item__info--description">{description}</p>
