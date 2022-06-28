37ab8a5c403491be5a29d9deaeb472152f111770
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-06-28 11:36:14 +0300

    add search query

--- src/constants/backend.js
+++ src/constants/backend.js
@@ -4,6 +4,8 @@ const BASE_URL = "https://api.punkapi.com/v2/beers";
 
 const GET_RANDOM_BEER = "/random";
 
+const SEARCH_BEER = "?beer_name=";
+
 const axios = Axios.create({ baseURL: BASE_URL });
 
 export { axios, GET_RANDOM_BEER };
