afaeddd2a395b41ad6ff467e7881a90321158d18
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-07-05 13:16:41 +0300

    declare svg module

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,6 +2,12 @@ declare module "*.mp3" {
   const value: any;
   export default value;
 }
+declare module "*.svg" {
+  import React = require("react");
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
 
 interface Window {
   ethereum: any;
