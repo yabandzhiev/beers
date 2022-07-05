4dd9a04ea882b971bc9a418c959cc82db9d47259
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-07-05 13:53:28 +0300

    css fix

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -7,11 +7,13 @@ import "./Content.scss";
 const Content = ({ filteredData }: dataProps) => {
   return (
     <div className="content">
-      {filteredData.length > 0
-        ? filteredData.map((beer: beerProps, index: number) => (
-            <BeerItem key={beer.name + index} beer={beer} />
-          ))
-        : "No Beers"}
+      {filteredData.length > 0 ? (
+        filteredData.map((beer: beerProps, index: number) => (
+          <BeerItem key={beer.name + index} beer={beer} />
+        ))
+      ) : (
+        <div className="no-beers">No Beers</div>
+      )}
     </div>
   );
 };
